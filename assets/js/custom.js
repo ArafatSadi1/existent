52bb676d3196b5882dadca79f70384a3d6670c42
ArafatSadi1 <arafatsadi1@gmail.com>
2022-10-05 17:02:29 +0600

    review card responsive

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,7 +1,19 @@
-var splide = new Splide(".splide", {
+var splide = new Splide(".splide-slider", {
   type: "loop",
   perPage: 3,
+  // gap for slider margin
+  gap: 30,
   focus: "center",
+  perMove: 1,
+  // for responsive
+  breakpoints : {
+    992: {
+      perPage: 2,
+    },
+    576: {
+      perPage: 1,
+    }
+  }
 });
 
 splide.mount();
